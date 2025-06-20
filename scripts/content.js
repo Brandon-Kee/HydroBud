5e3a1e2b959be8f2d3be3fa84a3d42a12829d8e7
bkee2 <bkee@seas.upenn.edu>
2025-06-20 17:28:11 -0400

    Cleaned up code

--- scripts/content.js
+++ scripts/content.js
@@ -210,31 +210,6 @@ function displayModal() {
             }
         );
     });
-
-    // Add event listener for close button
-    // const closeModalBtn = document.getElementById("closeModalBtn");
-    // closeModalBtn.addEventListener("click", () => {
-    //     const userInput = document.getElementById("userInput").value;
-    //     const water = parseInt(userInput);
-    //     console.log("User Input:", userInput); // Handle the input
-    //     document.body.removeChild(modalOverlay); // Remove modal from DOM
-    // });
-
-    // if (isNaN(water)) {
-    //     console.error("Invalid input: Please enter numeric values for amount of water.");
-    //     return;
-    // }
-
-    // console.log("User Input:", `${water} ml`);
-    // document.body.removeChild(modalOverlay); // Remove modal from DOM
-
-    // // Send message to background.js
-    // chrome.runtime.sendMessage({
-    //     action: "drinkWater",
-    //     amount: water,
-    // }, function (response) {
-    //     console.log("Response from background:", response);
-    // });
 }
 
 // Listen for messages from the popup script
@@ -246,83 +221,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     );
 });
 
-// Listen for messages from the popup script
-// chrome.runtime.onMessage.addListener(function (request, sendResponse) {
-//     if (request.message === "updatePlant") {
-//         console.log(`Message: ${request.message}`);
-//         updatePlant();
-//         sendResponse({ status: "Image update success" });
-//     }
-// });
 
-// function updatePlant() {
-//     // Get the selected image option from the storage mechanism
-//     chrome.storage.local.get(["selectedPlant", "plantState"], function (result) {
-//         // Get the selected image option from the storage mechanism
-//         const { selectedPlant = "plant1", plantState = "pot" } = result;
-//         console.log(`Selected plant: ${selectedPlant}, Plant state: ${plantState}`);
 
-//         // Get the plant element
-//         const plant = document.querySelector(".plant");
 
-//         // // Remove all classes from the plant element
-//         // plant.classList.remove(...plant.classList);
-
-//         // Add the plant class and the selected image class to the plant element
-//         plant.className = `plant ${selectedPlant} ${plantState}`;
-//     });
-// }
-
-// if (request.action === "aggressiveReminder") {
-//     // Handle aggressive mode behavior
-//     triggerAggressiveMode();
-//     sendResponse({ status: "Aggressive mode triggered" });
-// }
-// // Function to trigger aggressive mode behavior
-// function triggerAggressiveMode() {
-//     const plant = document.querySelector(".plant");
-//     if (plant) {
-//         // Make the plant very big
-//         plant.style.transform = "scale(3)"; // Increase the size by 3x
-//         plant.style.transition = "transform 0.5s ease"; // Smooth transition
-
-//         // Revert to normal size after a delay
-//         setTimeout(() => {
-//             plant.style.transform = "scale(1)"; // Back to normal size
-//         }, 5000); // 5 seconds delay
-//     }
-// }
-
-// WATER CONSUMPTION
-// let totalWater = 0;
-
-// // Initializes chrome.storage totalWater
-// document.addEventListener("DOMContentLoaded", function () {
-//     chrome.storage.local.get(["totalWater"], function (result) {
-//         // if total is NaN, initializes to 0
-//         if (result.totalWater == undefined) {
-//             chrome.storage.local.set({ totalWater: 0 });
-
-//             let midnight = new Date(); midnight.setHours(24, 0, 0, 0);
-//         } else {
-//             totalWater = result.totalWater;
-//         }
-
-//         document.getElementById("totalWater").textContent = totalWater.toFixed(1);
-//     })
-// })
-
-// // Drink button
-// document.getElementById("drinkButton").addEventListener("click", function () {
-//     let inputWater = parseFloat(document.getElementById("waterIn").value);
-
-//     if (!isNaN(inputWater)) {
-//         totalWater += inputWater;
-
-//         chrome.storage.local.set({ totalWater: totalWater }); // save totalWater to chrome storage
-//         document.getElementById("totalWater").textContent = totalWater.toFixed(1); // update displayed water
-//         document.getElementById("waterIn").value = ""; // clear input value
-//     }
-// });
-
-// TODO: ADD SKIP BUTTON
